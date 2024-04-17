607ce4e17488dc7c74ca7ac3ea4c79ab07ff9748
alexisabels <isabelsanchezalex@gmail.com>
2024-04-17 18:20:48 +0200

    path: ROOT,     element: Navigate to={HOME}

--- src/lib/routes.jsx
+++ src/lib/routes.jsx
@@ -1,28 +1,33 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import AuthPage from "../components/auth/AuthPage";
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 import Home from "../components/home";
 import TerminosYCondiciones from "../components/terms/TerminosYCondiciones";
-export const ROOT ="/";
+export const ROOT = "/";
 export const AUTH = "/auth";
-export const PROFILE ="/protected/profile/:id"
-export const PROTECTED ="/protected"
-export const HOME = "/protected/home"
-export const TERMSANDCONDITONS ="/terminosycondiciones"
+export const PROFILE = "/protected/profile/:id";
+export const PROTECTED = "/protected";
+export const HOME = "/protected/home";
+export const TERMSANDCONDITONS = "/terminosycondiciones";
 export const router = createBrowserRouter([
-    {path: ROOT, element: "Public Root"},
-    {path: AUTH, element: <AuthPage />},
-    {path: TERMSANDCONDITONS, element: <TerminosYCondiciones />},
-    {path: PROTECTED, element: <Layout />, children: [
-        {
-            path: HOME,
-            element: <Home />
-        },
-        {
-            path: PROFILE,
-            element: "User profile for a specific id"
-        },
-      
-    ]},
+  {
+    path: ROOT,
+    element: <Navigate to={HOME} replace />,
+  },
+  { path: AUTH, element: <AuthPage /> },
+  { path: TERMSANDCONDITONS, element: <TerminosYCondiciones /> },
+  {
+    path: PROTECTED,
+    element: <Layout />,
+    children: [
+      {
+        path: HOME,
+        element: <Home />,
+      },
+      {
+        path: PROFILE,
+        element: "User profile for a specific id",
+      },
+    ],
+  },
 ]);
-
