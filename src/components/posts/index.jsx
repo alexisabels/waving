e41fee3250dedea1548af2c40fc025365617f146
alexisabels <isabelsanchezalex@gmail.com>
2024-04-22 14:55:26 +0200

    Añadido estilo a las cards de post y un nombre y fecha por defecto

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,36 +1,55 @@
-/* eslint-disable react/prop-types */
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Box, Avatar, Stack } from "@mui/material";
 
 export default function Post({ post }) {
-  const {text} = post;
+  const { text } = post;
+  const username = "alex123";
+  const timestamp = "hace 1 minuto";
+
   return (
     <Paper
-
       elevation={0}
       sx={{
-        
-        width: '90%',
-        mb: 2,
-
-    
+        width: "100%",
+        my: 2,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
         border: 1,
-
+        borderColor: "grey.300",
         borderRadius: 5,
-        p: 2,
-        bgcolor: "rgb(230, 230, 230)",
-        borderColor: "rgb(230, 230, 230)",
-      
-   
-       
+        overflow: "hidden",
+        bgcolor: "grey.100",
       }}
     >
-  <Typography
-  variant="body1"
-  sx={{ 
-    wordBreak: "break-word",
-    whiteSpace: 'pre-line' 
-  }}
->
+      <Box
+        sx={{
+          bgcolor: "rgb(230, 230, 230)",
+
+          px: 2,
+          py: 1,
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ width: 38, height: 38, color: "black" }}></Avatar>{" "}
+        <Box sx={{ ml: 2 }}>
+          <Typography variant="subtitle2" component="div" noWrap>
+            {username}
+          </Typography>
+          <Typography variant="caption" color="text.secondary" noWrap>
+            {timestamp}
+          </Typography>
+        </Box>
+      </Box>
+      <Typography
+        variant="body1"
+        sx={{
+          wordBreak: "break-word",
+          whiteSpace: "pre-line",
+          p: 2,
+        }}
+      >
         {text}
       </Typography>
     </Paper>
