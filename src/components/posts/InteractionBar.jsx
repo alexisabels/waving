f7b36d16c46f0e935dec3bc9680ce8ffc0d86eab
alexisabels <isabelsanchezalex@gmail.com>
2024-05-28 12:54:27 +0200

    InteractionBar fix

--- src/components/posts/InteractionBar.jsx
+++ src/components/posts/InteractionBar.jsx
@@ -1,9 +1,35 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import { Stack } from "@mui/material";
 import LikeButton from "./Interaction/LikeButton";
 import CommentButton from "./Interaction/CommentButton";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
-export default function InteractionBar({ postId, commentsCount }) {
+export default function InteractionBar({ postId }) {
+  const [commentsCount, setCommentsCount] = useState(0);
+  const db = getFirestore();
+
+  useEffect(() => {
+    const fetchCommentsCount = async () => {
+      try {
+        const commentsCollectionRef = collection(db, "comments");
+        const q = query(commentsCollectionRef, where("postId", "==", postId));
+        const querySnapshot = await getDocs(q);
+        setCommentsCount(querySnapshot.size);
+      } catch (error) {
+        console.error("Error fetching comments count: ", error);
+      }
+    };
+
+    fetchCommentsCount();
+  }, [postId, db]);
+
   return (
     <div>
       <Stack
@@ -13,7 +39,7 @@ export default function InteractionBar({ postId, commentsCount }) {
         sx={{ width: "85%", px: 2, pt: 0, pb: 2 }}
       >
         <LikeButton postId={postId} />
-        <CommentButton commentsCount={commentsCount} />
+        <CommentButton commentsCount={commentsCount} postId={postId} />
       </Stack>
     </div>
   );
